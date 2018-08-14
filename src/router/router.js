fa68757aa360b0970c6ed9d74c0c7e5be5aff691
liushengpeng <328232234@qq.com>
2018-08-14 15:00:03 +0800

    save

--- src/router/router.js
+++ src/router/router.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import {BrowserRouter as Router, Link, Route, Switch} from 'react-router-dom'
+
 import Home from 'pages/Home/Home'
 import Page1 from 'pages/Page1/Page1'
+import Counter from 'pages/Counter/Counter'
+import UserInfo from 'pages/UserInfo/UserInfo'
 
 function getRouter() {
   return (
@@ -10,10 +13,14 @@ function getRouter() {
         <ul>
           <li><Link to="/">首页</Link></li>
           <li><Link to="/page1">Page1</Link></li>
+          <li><Link to="/counter">Counter</Link></li>
+          <li><Link to="/userinfo">UserInfo</Link></li>
         </ul>
         <Switch>
           <Route exact path="/" component={Home}/>
           <Route path="/page1" component={Page1}/>
+          <Route path="/counter" component={Counter}/>
+          <Route path="/userinfo" component={UserInfo}/>
         </Switch>
       </div>
     </Router>
